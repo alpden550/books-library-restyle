63d08e0342fadca222d8ccc71f51e9b77bb78334
alpden550 <alpden550@gmail.com>
2020-01-05 11:20:30 +0300

    Added configuration for linter.
    Formatted parse.py.

--- parse.py
+++ parse.py
@@ -1,43 +1,45 @@
 import argparse
 import logging
-
-from parser.parse_tululu_category import parse_category, SCI_FI_LAST_PAGE
+from parser.parse_tululu_category import SCI_FI_LAST_PAGE, parse_category
 
 
 def create_parser():
     parser = argparse.ArgumentParser(
-        description="Parse sci-fi book library and dowload books in txt format."
+        description='Parse sci-fi book library and dowload books in txt format.',
     )
     parser.add_argument(
-        "-s",
-        "--start_page",
-        help="Starting page for parsing, default is 1.",
+        '-s',
+        '--start_page',
+        help='Starting page for parsing, default is 1.',
         type=int,
-        choices=range(1, 702),
-        metavar="From 1 to 701",
+        choices=range(1, SCI_FI_LAST_PAGE + 1),
+        metavar='From 1 to 701',
     )
     parser.add_argument(
-        "-e",
-        "--end_page",
+        '-e',
+        '--end_page',
         type=int,
-        help="Ending page for parsing, default is 2.",
-        choices=range(1, 703),
-        metavar="From 1 to 702",
+        help='Ending page for parsing, default is 2.',
+        choices=range(1, SCI_FI_LAST_PAGE + 2),
+        metavar='From 1 to 702',
     )
     return parser.parse_args()
 
 
 def main(start_page=None, end_page=None):
     start = parser.start_page or 1
-    end = parser.end_page or SCI_FI_LAST_PAGE+1
+    end = parser.end_page or SCI_FI_LAST_PAGE + 1
     parse_category(start, end)
 
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, format="%(levelname)s %(message)s")
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s %(message)s')
     parser = create_parser()
     if not any((parser.start_page, parser.end_page)):
-        logging.warning(
-            "Attention!! \n\nYou are downloading more than 17000 books.\n\nIt will take extremely many time..\n\n"
-        )
+        message = """
+        Attention!!,\n\n
+        You are downloading more than 17000 books.,\n\n
+        It will take extremely many time..\n\n
+        """
+        logging.warning(message)
     main(parser.start_page, parser.end_page)
