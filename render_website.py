ee70aede98f6d717a5e8a01c7336bfb69ac3cf62
alpden550 <alpden550@gmail.com>
2020-02-15 09:20:01 +0300

    UPdated requitements.txt.

--- render_website.py
+++ render_website.py
@@ -22,13 +22,13 @@ def main(pdges_dir='pages'):
     Path(pdges_dir).mkdir(exist_ok=True)
     books_data = json.loads(Path('sci-fi.json').read_text())
     chunked_books = get_books_chunks(books_data)
-    for number, books in enumerate(chunked_books, start=1):
+    for chunk_number, books in enumerate(chunked_books, start=1):
         rendered_page = template.render(
             books=books,
             page_numbers=len(chunked_books),
-            current_page=number,
+            current_page=chunk_number,
         )
-        Path('pages/index{number}.html'.format(number=number)).write_text(rendered_page)
+        Path('pages/index{number}.html'.format(number=chunk_number)).write_text(rendered_page)
 
 
 if __name__ == '__main__':
